9f90f02f389eb012a7f5247a10b79eec08b033e3
Zach Shipko <zachshipko@gmail.com>
2019-11-14 22:18:32 -0800

    Add filter::dlopen

--- src/filter.rs
+++ src/filter.rs
@@ -1,6 +1,8 @@
 pub use dlopen::wrapper::*;
 pub use dlopen_derive::*;
 
+pub use dlopen;
+
 pub fn load<T: WrapperApi>(path: &str) -> Result<Container<T>, dlopen::Error> {
     unsafe { Container::load(path) }
 }
